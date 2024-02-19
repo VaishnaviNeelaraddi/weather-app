95ad082096a0482c0414bfd86a1e7a7b364d32af
VaishnaviNeelaraddi <vaishnavineelaraddi05@gmail.com>
2024-02-19 23:57:27 +0530

    init

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7888f0024emsh6c4c5670b88aebbp12e499jsna03f3b5a5f03", // enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "06a3864889d31f72b3dfa6e900eba6eb"; // enter your key from openweather API
